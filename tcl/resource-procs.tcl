8c0d5320903b9d5f98f112ee27fc372999b6b673
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2024-03-20 10:21:01 +0000

    JS upstream updates
    - Updated upstream library to 11.4.0
    - Bumped version to 0.5

--- tcl/resource-procs.tcl
+++ tcl/resource-procs.tcl
@@ -20,14 +20,14 @@ namespace eval ::highcharts {
     # configuration file:
     #
     #    ns_section ns/server/${server}/acs/highcharts
-    #        ns_param HighchartsVersion 11.3.0
+    #        ns_param HighchartsVersion 11.4.0
     #
     #  For new versions, checkout https://cdnjs.com/
     #
     set ::highcharts::version [parameter::get \
                                    -package_id $package_id \
                                    -parameter HighchartsVersion \
-                                   -default 11.3.0]
+                                   -default 11.4.0]
 
     ad_proc ::highcharts::resource_info {
         {-version ""}

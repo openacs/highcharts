1a7be87b7980292aefc2326dad5e8bf09635acd9
Gustaf Neumann <gustaf.neumann@wu-wien.ac.at>
2025-04-29 14:15:55 +0000

    bumped highcharts version to 12.2.0

--- tcl/resource-procs.tcl
+++ tcl/resource-procs.tcl
@@ -19,12 +19,12 @@ namespace eval ::highcharts {
     # configuration file:
     #
     #    ns_section ns/server/${server}/acs/highcharts
-    #        ns_param HighchartsVersion 12.1.2
+    #        ns_param HighchartsVersion 12.2.0
     #
     set parameter_info {
         package_key highcharts
         parameter_name HighchartsVersion
-        default_value 12.1.2
+        default_value 12.2.0
     }
 
     ad_proc ::highcharts::resource_info {
